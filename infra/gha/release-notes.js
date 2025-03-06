546e50b8e2208bedfd7bb0c195cf857d90859c24
faustofjunqueira <fausto.junqueira@stone.com.br>
2025-03-05 21:17:54 -0300

    codeowner

--- infra/gha/release-notes.js
+++ infra/gha/release-notes.js
@@ -1,46 +1,50 @@
 // inputs
 // - tag criada <namespace>/v<semver>
 
-function getVersionsFromTag(tag) {
-    const [namespace, version] = tag.split('/')
-    const [_, semver] = version.split('v')
-    const [major, minor, patchWithPreRelease] = semver.split('.');
-    const [patch, preRelease] = patchWithPreRelease.split('-');
+function parseRefName(refName) {
+  const [namespace, version] = refName.split("/");
+  const [_, semver] = version.split("v");
+  const [major, minor, patchWithPreRelease] = semver.split(".");
+  const [patch, preRelease] = patchWithPreRelease.split("-");
 
-    return {namespace, version, semver: {semver, major, minor, patch, preRelease}}
+  return {
+    namespace,
+    version,
+    semver: { semver, major, minor, patch, preRelease },
+  };
 }
 
-function checkIfTagIsSemver(tag) {
-    const semverRegex = /^([a-zA-Z0-9-_]+)\/v(\d+)\.(\d+)\.(\d+)(?:-([0-9]+))?$/;
-    return semverRegex.test(tag);
+function checkIfTagIsSemver(refName) {
+  const semverRegex = /^([a-zA-Z0-9-_]+)\/v(\d+)\.(\d+)\.(\d+)(?:-([0-9]+))?$/;
+  return semverRegex.test(tag);
 }
 
-module.exports = ({github, context, core, glob}) => {
+module.exports = ({ github, context, core, glob }) => {
+  try {
     const ref = context.ref;
-    const [, type, ...refsName] = ref.split('/')
-    const refName = refsName.join('/');
-    
+    const [, type, ...refsName] = ref.split("/");
+    const refName = refsName.join("/");
+
     console.log(refName);
 
     if (type == 'heads') {
         throw new Error('This action only works with tags');
     }
-    
+
     if (!checkIfTagIsSemver(refName)) {
-        throw new Error('ref is not a valid semver tag');
+      throw new Error("ref is not a valid semver tag");
     }
-    
-    
-    console.log(getVersionsFromTag("namespace/v1.2.3"))
-    console.log(getVersionsFromTag("namespace/v1.2.3-0"))
-    console.log(getVersionsFromTag("namespace/v1.2.3-1"))
+
+    console.log(parseRefName("namespace/v1.2.3"));
+    console.log(parseRefName("namespace/v1.2.3-0"));
+    console.log(parseRefName("namespace/v1.2.3-1"));
 
     // Verifica a tag
     // Verifica se major, minor, patch ou pre-release
-        // se minor: a tag X.X.0 se termina com zero é minor
-        // se patch: a tag X.X.X se termina com numero é patch
-        // se major: a tag X.0.0 se termina com zero é major
-        // se pre-release: a tag X.X.X-<pre-release> se termina com pre-release é pre-release
+    // se minor: a tag X.X.0 se termina com zero é minor
+    // se patch: a tag X.X.X se termina com numero é patch
+    // se major: a tag X.0.0 se termina com zero é major
+    // se pre-release: a tag X.X.X-<pre-release> se termina com pre-release é pre-release
 
     // se for pre-release, não cria changelog
     // deve buscar TODOS os commits do mesmo scope
@@ -49,4 +53,7 @@ module.exports = ({github, context, core, glob}) => {
     // Se prerelease o PREVIOUS_TAG => É a ultima hotfix ou minor gerada!
     // Criar uma sessão para Hotfixes, no caso de minor e major. Esses são todos os hotfixes gerados para minor anterior
     // Separa os logs por seção!
-}
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
