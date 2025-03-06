092b7357f3c8aa85c884b426a56c617c222a5c62
faustofjunqueira <fausto.junqueira@stone.com.br>
2025-03-06 01:01:07 -0300

    codeowner

--- infra/gha/release-notes.js
+++ infra/gha/release-notes.js
@@ -274,7 +274,7 @@ async function createsReleaseNotes({ github, context, core, glob }) {
   try {
     // const ref = context.ref;
     const repoName = [context.repo.owner, context.repo.repo].join("/");
-    const ref = "refs/tags/namespace/v1.5.0";
+    const ref = "refs/tags/namespace/v1.6.0";
     const [, type, ...refsName] = ref.split("/");
     const refName = refsName.join("/");
 
